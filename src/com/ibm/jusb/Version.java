ed2d28ef6c8a0a7fdc6c0070dddd8d444fa58eba
Dan Streetman <ddstreet@us.ibm.com>
2002-08-09 19:59:45 +0000

    updated implementation version number to 0.9.2 (API version number unchanged)

--- src/com/ibm/jusb/Version.java
+++ src/com/ibm/jusb/Version.java
@@ -54,5 +54,5 @@ public final class Version
 	public static String getRiVersion() { return VERSION_PLATFORM_INDEPENDENT_RI; }
 
 	private static final String VERSION_API = "0.9.1";
-	private static final String VERSION_PLATFORM_INDEPENDENT_RI = "0.9.1";
+	private static final String VERSION_PLATFORM_INDEPENDENT_RI = "0.9.2";
 }
