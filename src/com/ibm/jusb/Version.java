784bcde768750f1376cedf3d89f30473f0a4626d
Dan Streetman <ddstreet@us.ibm.com>
2005-03-01 01:42:39 +0000

    updated version to 1.0.0, yea!

--- src/com/ibm/jusb/Version.java
+++ src/com/ibm/jusb/Version.java
@@ -54,6 +54,6 @@ public class Version
 	 */
 	public static String getRiVersion() { return VERSION_PLATFORM_INDEPENDENT_RI; }
 
-	private static final String VERSION_API = "0.10.3";
-	private static final String VERSION_PLATFORM_INDEPENDENT_RI = "0.10.4";
+	private static final String VERSION_API = "1.0.0";
+	private static final String VERSION_PLATFORM_INDEPENDENT_RI = "1.0.0";
 }
